8ffa833a738840eb29e57229761596b10aabf8f4
Rajiv Roy <rajivpune18@gmail.com>
2025-02-17 17:28:28 +0530

    updated region code to IN

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 const GOOGLE_API_KEY = "AIzaSyBBzgsEg7vK7VT3mROvqbk0C5bVGy63K5U";
-export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key=${GOOGLE_API_KEY}`;
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
