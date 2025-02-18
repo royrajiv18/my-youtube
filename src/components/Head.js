b507cd8ce17f09ecfcd865a6d8806f10eecc786b
Rajiv Roy <rajivpune18@gmail.com>
2025-02-18 11:07:41 +0530

    show search suggestions

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,6 +4,8 @@ import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
+  const [suggestions, setSuggestions] = useState([]);
+
   const [searchQuery, setSearchQuery] = useState("");
 
   // ************DEBOUNCING ******
@@ -14,7 +16,7 @@ const Head = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       getSearchSuggestions();
-    }, 3000);
+    }, 200);
 
     return () => {
       clearTimeout(timer);
@@ -24,7 +26,7 @@ const Head = () => {
   const getSearchSuggestions = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    console.log("search -", json.items[0]);
+    setSuggestions(json.items);
   };
 
   // ******************************************
@@ -50,20 +52,32 @@ const Head = () => {
       </div>
 
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-600 rounded-l-full border-r-0 p-2"
-          placeholder="Search"
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="border border-gray-600 rounded-r-full px-5 py-2 bg-gray-100">
-          <img
-            className="h-5 inline-block"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-aKJ9AbwvP0MGcEyOeOw_4XGgMktjq1WMsA&s"
-            alt="search"
+        <div>
+          <input
+            className="w-1/2 border border-gray-600 rounded-l-full border-r-0 p-2"
+            placeholder="Search"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
-        </button>
+          <button className="border border-gray-600 rounded-r-full px-5 py-2 bg-gray-100">
+            <img
+              className="h-5 inline-block"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-aKJ9AbwvP0MGcEyOeOw_4XGgMktjq1WMsA&s"
+              alt="search"
+            />
+          </button>
+          <div className="fixed bg-white py-2 px-5 w-[29rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.length > 0 &&
+                suggestions.map((s) => (
+                  <li key={s} className="py-2 shadow-sm hover:bg-gray-200">
+                    🔎 {s?.snippet?.title}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div className="col-span-1">
