6f9402e54ccec46bd4c82a1ad59cf89edc0a1a02
Rajiv Roy <rajivpune18@gmail.com>
2025-02-19 12:00:50 +0530

    live chat feature completed

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,22 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState();
+  const dispatch = useDispatch();
+
+  const chatMessages = useSelector((store) => store.chat.messages);
+
   useEffect(() => {
     const i = setInterval(() => {
-      console.log("API polling");
-    }, 2000);
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: makeRandomMessage(20),
+        })
+      );
+      setLiveMessage("");
+    }, 1500);
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="w-full h-[600px] p-2 ml-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage name="Rajiv" message="This is a sample chat 🔥" />
-      <ChatMessage name="Rajiv" message="This is a sample chat 🔥" />
-      <ChatMessage name="Rajiv" message="This is a sample chat 🔥" />
-      <ChatMessage name="Rajiv" message="This is a sample chat 🔥" />
-    </div>
+    <>
+      <div className="w-full h-[600px] p-2 ml-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        {chatMessages.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+      <form
+        className="p-2 ml-2 w-full border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Rajiv",
+              message: liveMessage,
+            })
+          );
+        }}
+      >
+        <input
+          className="p-2 w-72 border border-black"
+          type="text"
+          placeholder="Chat"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        ></input>
+        <button className="px-2 mx-2 bg-green-300">Send</button>
+      </form>
+    </>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 const GOOGLE_API_KEY = "AIzaSyBBzgsEg7vK7VT3mROvqbk0C5bVGy63K5U";
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=10&key=${GOOGLE_API_KEY}`;
 export const YOUTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${GOOGLE_API_KEY}&q=`;
+export const LIVE_CHAT_COUNT = 10;
